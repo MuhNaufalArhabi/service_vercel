0516cf7667639afa4df9c309cc3238a93d829f05
naufal <naufal.arhabi@bvarta.com>
2024-06-25 16:19:41 +0700

    fix:cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const err = require('./middlewares/errorHandler');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
-app.use(cors({origin: '*'}))
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
